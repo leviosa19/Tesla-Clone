2070d3fe763654ecdc9a324d7b6ef4edd24af187
Rushi1618 <31608167+Rushi1618@users.noreply.github.com>
2020-12-15 10:30:31 +0530

    Updated

--- App.js
+++ App.js
@@ -5,20 +5,20 @@ import CarsList from './components/CarsList';
 import Header from './components/Header';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Header />
-      <CarsList />
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<Header />
+			<CarsList />
+			<StatusBar style="auto" />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    // backgroundColor: '#fff',
-    // alignItems: 'center',
-    // justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		// backgroundColor: '#fff',
+		// alignItems: 'center',
+		// justifyContent: 'center',
+	},
 });
